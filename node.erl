0f04701fa78753f376c40bd09ace0fa83101153f
Quentin Nater <37473138+qnater@users.noreply.github.com>
2021-12-20 16:03:04 +0000

    ring server/client

--- node.erl
+++ node.erl
@@ -9,13 +9,35 @@
 %Timeout
 -define(Timeout, 10000).
 
-deploy(N) ->
-  deploy(N, nil, []).
 
+% deploy(N)
+% Id : Id of the node 
+%
+% Deploy a new node
+% Quentin Nater - 12/21
+deploy(Id) ->
+  deploy(Id, nil, []).
+
+
+% deploy(Id, Peer, Store)
+% Id    : Id of the node 
+% Peer  : Process to connect
+% Store : Current keys of the node
+%
+% Deploy a new node with information
+% Quentin Nater - 12/21
 deploy(Id, Peer, Store) ->
   timer:start(),
   spawn(fun() -> init(Id, Peer, Store) end).
 
+
+% init(Id, Peer, Store)
+% Id    : Id of the node 
+% Peer  : Process to connect
+% Store : Current keys of the node
+%
+% Initialize and connect with other nodes
+% Quentin Nater - 12/21
 init(Id, Peer, Store) ->
   Predecessor = nil,
   {ok,Successor} = connect(Id, Peer),
@@ -23,13 +45,31 @@ init(Id, Peer, Store) ->
   node(Id,Predecessor,Successor, Store).
 
 
+% handle_stabilize()
+% 
+% Start each X second the stabilization
+% Quentin Nater - 12/21
 handle_stabilize() ->
   timer:send_interval(?TimeToStabilize, self(), stabilize).
 
 
+% connect(Id, nil)
+% Id    : Id of the node 
+% nil   : No connection
+%
+% Start a new connection
+% Quentin Nater - 12/21
 connect(Id, nil) ->
   % When we enter, we are our own successor
   {ok, {Id, self()}};
+
+
+% connect(_, Peer)
+% _     : Id of the node 
+% nil   : No connection
+%
+% Start a connection
+% Quentin Nater - 12/21
 connect(_, Peer) ->
   Qref = make_ref(),
   Peer ! {key, Qref, self()},
@@ -40,75 +80,119 @@ connect(_, Peer) ->
     io:format("Time out !~n")
   end.
 
-  
+% node(Id,Predecessor,Successor,Store)
+% Id          : Id of the node 
+% Predecessor : Predecessor of the node
+% Successor   : Successor of the node
+% Store : Current keys of the node
+%
+% Handle the communication between nodes
+% Quentin Nater - 12/21
 node(Id,Predecessor,Successor,Store) ->
   receive
-  % ... Actions ......................................
-  {key,Qref,Peer} -> % Peer : node asking, QRef : the key / a peer needs to know our key
-    Peer ! {Qref, Id}, 
-    node(Id, Predecessor, Successor, Store);
-  {notify, New} -> % A new node informs us of its existence
-    {Pred, Keep} = notify(New, Id, Predecessor, Store),
-    node(Id, Pred, Successor, Keep);
-  {request, Peer} -> % A predecessor needs to know our predecessor
-    request(Peer, Predecessor),
-    node(Id, Predecessor, Successor, Store);
-  {status, Pred} -> % Our successor informs us about its predecessor
-    Succ = stabilize(Pred, Id, Successor),
-    node(Id, Predecessor, Succ, Store);
-  stabilize -> % stabilize the ring
-    {_, Spid} = Successor,
-    Spid ! {request, self()},
-    node(Id, Predecessor, Successor, Store);
+    % ... Actions ......................................
+    {key,Qref,Peer} -> % Peer : node asking, QRef : the key / a peer needs to know our key
+      Peer ! {Qref, Id}, 
+      node(Id, Predecessor, Successor, Store);
+    {notify, New} -> % A new node informs us of its existence
+      {Pred, Keep} = notify(New, Id, Predecessor, Store),
+      node(Id, Pred, Successor, Keep);
+    {request, Peer} -> % A predecessor needs to know our predecessor
+      request(Peer, Predecessor),
+      node(Id, Predecessor, Successor, Store);
+    {status, Pred} -> % Our successor informs us about its predecessor
+      Succ = stabilize(Pred, Id, Successor),
+      node(Id, Predecessor, Succ, Store);
+    stabilize -> % stabilize the ring
+      {_, Spid} = Successor,
+      Spid ! {request, self()},
+      node(Id, Predecessor, Successor, Store);
 
-  % ... Informations/Tests ...........................
-  {add, Key, Value, Qref, Client} ->
-    Added = add(Key, Value, Qref, Client, Id, Predecessor, Successor, Store),
-    node(Id, Predecessor, Successor, Added);
-  {lookup, Key, Qref, Client} ->
-    lookup(Key, Qref, Client, Id, Predecessor, Successor, Store),
-    node(Id, Predecessor, Successor, Store);
-  {handover, Elements} ->
-    Merged = lists:keymerge(1, Elements, Store),
-    node(Id, Predecessor, Successor, Merged);
-  logs ->
-    io:format("RESULT> ~w -> Pred:~w Succ:~w STORE:~w~n",[Id,Predecessor,Successor,Store]),
-    node(Id, Predecessor, Successor, Store)
+    % ... Informations/Tests/Actions ...........................
+    {add, Key, Value, Qref, Client} ->
+      Added = add(Key, Value, Qref, Client, Id, Predecessor, Successor, Store),
+      node(Id, Predecessor, Successor, Added);
+    {lookup, Key, Qref, Client} ->
+      lookup(Key, Qref, Client, Id, Predecessor, Successor, Store),
+      node(Id, Predecessor, Successor, Store);
+    {handover, Elements} ->
+      Merged = lists:keymerge(1, Elements, Store),
+      node(Id, Predecessor, Successor, Merged);
+    logs ->
+      io:format("RESULT> ~w -> Pred:~w Succ:~w STORE:~w~n",[Id,Predecessor,Successor,Store]),
+      node(Id, Predecessor, Successor, Store)
   end.
 
+
+% add(Key, Value, Qref, Client, Id, {Pkey, _}, {_, Spid}, Store)
+% Key         : Key to handle
+% Value       : Value of the key
+% Qref        : Current reference
+% Client      : Client who asks the adding
+% Id          : Id of the node 
+% Predecessor : Predecessor of the node
+% Successor   : Successor of the node
+% Store       : Current keys of the node
+%
 % add a key value pair, return the updated store
+% Quentin Nater - 12/21
 add(Key, Value, Qref, Client, Id, {Pkey, _}, {_, Spid}, Store) ->
   case key:between(Key, Pkey, Id) of
       true ->
-          Client ! {Qref, keyok},
+          Client ! {l, Qref, keyok},
           lists:keystore(Key, 1, Store, {Key, Value}); 
       false ->
           Spid ! {add, Key, Value, Qref, Client},
           Store
   end.
 
+% lookup(Key, Qref, Client, Id, {Pkey, _}, Successor, Store)
+% Key         : Key to handle
+% Qref        : Current reference
+% Client      : Client who asks the adding
+% Id          : Id of the node 
+% Predecessor : Predecessor of the node
+% Successor   : Successor of the node
+% Store       : Current keys of the node
+%
 % return a tuple {Key, Value} or the atom false
+% Quentin Nater - 12/21
 lookup(Key, Qref, Client, Id, {Pkey, _}, Successor, Store) ->
     case key:between(Key, Pkey, Id) of
         true ->
             Result = lists:keyfind(Key, 1, Store),
-            io:format(">> ~w (L) : Lookup resut : ~w~n",[Id,Result]),
+            io:format(">> ~w (L) : Lookup result for ~w : ~w~n",[Id,Key,Result]),
             {_,R} = Result,
-            Client ! {Qref,R};
+            Client ! {l, Qref,R};
         false ->
             {_, Spid} = Successor,
             Spid ! {lookup, Key, Qref, Client}
         end.
 
 
-
+% handover(Store, Nkey, Npid)
+% Store       : Current keys of the node
+% Nkey        : Id of the node
+% Npid        : Process of the node
+%
+% Give the key to some other node
+% Quentin Nater - 12/21
 handover(Store, Nkey, Npid) ->
   {Leave, Keep} = lists:partition(fun({K,_}) -> K =< Nkey end, Store),
   Npid ! {handover, Leave},
   Keep.
 
 
+
+% notify({Nkey, Npid}, Id, Predecessor, Store)
+% Nkey        : Id of the node
+% Npid        : Process of the node
+% Id          : Id of the node 
+% Predecessor : Predecessor of the node
+% Store       : Current keys of the node
+%
 % A way for a node to make a friendly proposal that it might be our proper predecessor. 
+% Quentin Nater - 12/21
 notify({Nkey, Npid}, Id, Predecessor, Store) ->
     case Predecessor of
         nil ->  
@@ -125,6 +209,14 @@ notify({Nkey, Npid}, Id, Predecessor, Store) ->
             end
     end.
 
+
+
+% request(Peer, Predecessor)
+% Peer        : Process to connect
+% Predecessor : Predecessor of the node
+%
+% request information about the current status
+% Quentin Nater - 12/21
 request(Peer, Predecessor) ->
  case Predecessor of
   nil ->
@@ -133,10 +225,18 @@ request(Peer, Predecessor) ->
     Peer ! {status, {Pkey, Ppid}}
   end.
 
-stabilize(Pred, Id, Successor) ->
+
+% stabilize(stabilize, Predecessor)
+% Predecessor : Predecessor of the node
+% Id          : Id of the node 
+% Successor   : Successor of the node
+%
+% communicate to update the status of the current predecessor and successor
+% Quentin Nater - 12/21
+stabilize(Predecessor, Id, Successor) ->
   {Skey, Spid} = Successor,
 
-  case Pred of
+  case Predecessor of
     nil ->
       Spid ! {notify, {Id, self()}},
       Successor;
@@ -153,7 +253,7 @@ stabilize(Pred, Id, Successor) ->
       false ->
           Xpid ! {request, self()},
           io:format("~w (U) : updates successor from ~w to ~w ~n",[Id, Skey, Xkey]),
-          Pred;
+          Predecessor;
       true -> 
           Spid ! {notify, {Id, self()}},
           io:format("~w (N) : sending notify to ~w (pred : ~w)~n",[Id, Skey, Xkey]),
@@ -161,12 +261,28 @@ stabilize(Pred, Id, Successor) ->
       end
   end.
 
+
+% addEntry(Key, Value, NodePid, Client)
+% Key         : Key to handle
+% Value       : Value of the key
+% NodePid     : Id of a node inside the ring
+% Client      : Client who asks the adding
+%
+% Add a new key in the ring
+% Quentin Nater - 12/21
 addEntry(Key, Value, NodePid, Client) ->
   Qref = make_ref(),
   NodePid ! {add, Key, Value, Qref, Client},
   Qref.
 
 
+% addEntry(Key, Value, NodePid, Client)
+% Key         : Key to handle
+% NodePid     : Id of a node inside the ring
+% Client      : Client who asks the adding
+%
+% Get the value of a key inside the ring
+% Quentin Nater - 12/21
 getEntry(Key, NodePid, Client) ->
   Qref = make_ref(),
   NodePid ! {lookup, Key, Qref, Client},
--- ring.erl
+++ ring.erl
@@ -1,136 +1,312 @@
 -module(ring).
--export([start/0, startRing/1, startRing/3, stop/1, listener/1, lookFor/2, addKey/3, handling/1, addNode/3, addNode/1, lookFor/1, addKey/2, info/0, log/3,logs_in_time/0]).
+-export([start/0, startRing/1, startRing/3, stop/1, listener/2, handling/1, generateKey/1, generateKey/0, getInfo/2, receiver/0, init/0, login/2, login/3, lookFor/3, postKey/3, postKey/4]).
 
 -define(VERBOSITY, 0).
+-define(Mbit,64).
 -define(TimeToStabilize, 20000).
 
+%Timeout
+-define(Timeout, 10000).
+
+% start()
+%
+% use to start and build a new ring
+% Quentin Nater - 12/21
 start() ->
     timer:start(),
-    io:format(".......Main Process : ~w...........~n........................................~n", [self()]),
+    register(myring, self()),  % save the main process
+    io:format(".......Main Process : ~w...........~n.......Size of the ring : ~w............~n........................................~n~n", [self(),?Mbit]),
     startRing(4).
 
-
+% startRing(N)
+% N : Number of process
+%
+% use to start and build a new ring of the first N processes
+% Quentin Nater - 12/21
 startRing(N) ->
-    Key = key:generate(),
+    io:format("....INITIALIZATION........................................~n"),
+    Key = generateKey(),
     StarterPid  = node:deploy(Key, nil, []),
     io:format("..startRing (StarterPid) : ~w ~w....................~n", [Key, StarterPid]),
     startRing(N-1, StarterPid, [{Key, StarterPid}]).
 
 
+% startRing(N)
+% N     : Number of process
+% Spid  : Reference process in the ring
+% Nodes : Array of all processes (nodes) inside the ring
+%
+% use to start and build a new ring of N processes
+% Quentin Nater - 12/21
 startRing(N,Spid,Nodes) when N > 0 ->
-    Key = key:generate(),
+    Key = generateKey(Nodes),
     Pid = node:deploy(Key, Spid, []),
     io:format("..startRing (StarterPid) : ~w ~w....................~n", [Key, Pid]),
     timer:sleep(1000),
     startRing(N-1,Spid,[{Key,Pid}|Nodes]);
     
-   
+  
+% startRing(_,_,Nodes)
+% _     : last process
+% _     : last process in the ring
+% Nodes : Array of all processes (nodes) inside the ring
+%
+% use to build and test the created ring
+% Quentin Nater - 12/21
 startRing(_,_,Nodes) -> 
-  % KEY FOR TESTS.......................................
-  KeyAlpha  = key:generate(),
-  KeyBeta   = KeyAlpha + 1,
-  KeyGamma  = key:generate(),
-  KeyDelta  = key:generate(),
+
+   io:format("~n....TEST/INTERACTION......................................~n"),
+
 
   % DYNAMIC INSERT OF KEY...............................
-  addKey(KeyAlpha, 31415),
-  addKey(KeyBeta, 9265),
-  addKey(KeyGamma, 3596),
+  myring ! {h,addkey,9265,nil},
+  myring ! {h,addkey,3596,nil},
 
   % DYMAMIC INSTER OF NODE.............................
-  addNode(KeyDelta),
+  myring ! {h,addnode,nil},
+
   
-  % LOOK FOR A KEY.....................................
-  lookFor(KeyAlpha),
-
   % LOGS...............................................
-  info(),
+  myring ! {info,nil},
 
-  logs_in_time(),
-
+  % LISTENER...........................................
   handling(Nodes),
 
   Nodes.
 
-logs_in_time() ->
-  timer:send_interval(?TimeToStabilize, self(), info).
-
+% handling(Nodes)
+% Nodes : Array of all processes (nodes) inside the ring
+%
+% use to handle all messages inside the main process
+% Quentin Nater - 12/21
 handling(Nodes) ->
-  {_,Process} = lists:last(Nodes),
+  {_,Process} = lists:last(Nodes), % reference
   timer:sleep(500),
   io:format("..wait........................~n", []),
+  
   receive
-    {h,lookfor,Key} ->
-      node:getEntry(Key, Process, self()),
-      listener(Key),
-      timer:sleep(1000),
+    {h, lookfor, Key, Client} -> % look for a key
+      node:getEntry(Key, Process, myring),
+      listener(Key, Client),
+      {myring,Client} ! {lookkeys, Key}, % inform the client
+      timer:sleep(1200),
       handling(Nodes);
-    {h,addkey,Key,Value} ->
-      node:addEntry(Key, Value, Process, self()),
-      listener(Key),
+
+     {h, addkey, Value, Client} -> % add a new key and generate a new ID
+      Key = generateKey(Nodes),
+      node:addEntry(Key, Value, Process, myring),
+      listener(Key, Client),
+      {myring,Client} ! {get, keys, Key}, % inform the client
       timer:sleep(200),
       handling(Nodes);
-    {h,addnode,Key} ->
+
+   
+    {h, addkey, Key, Value, Client} -> % add a new key
+      node:addEntry(Key, Value, Process, myring),
+      listener(Key, Client),
+      {myring,Client} ! {get, keys, Key}, % inform the client
+      timer:sleep(200),
+      handling(Nodes);
+
+  
+    {h, addnode, Client} -> % add a new node and generate a new ID
+      Key = generateKey(Nodes),
       Pid = node:deploy(Key, Process, []),
-      UpdatedNodes = [{Key,Pid}|Nodes],
-      timer:sleep(1000),
+      UpdatedNodes = [{Key,Pid}|Nodes], % update the current array of nodes
+      timer:sleep(1500),
+      {myring,Client} ! {getnodes, UpdatedNodes}, % inform the client
       handling(UpdatedNodes);
-    info ->
-      [Y!logs || {_,Y} <- Nodes],
+
+    {h, addnode, Key, Client} -> % add a new node
+      Pid = node:deploy(Key, Process, []),
+      UpdatedNodes = [{Key,Pid}|Nodes], % update the current array of nodes
+      timer:sleep(1500),
+      {myring,Client} ! {getnodes, UpdatedNodes}, % inform the client
+      handling(UpdatedNodes);
+
+    {info, Client} -> % display all information about the ring
+      [Y!logs || {_,Y} <- Nodes], % ask all nodes for logs
+      {myring,Client} ! {get, Nodes},  % inform the client
       io:format(">> Nodes : ~w ~n", [Nodes]),
-      log(2, "(LOGS) [~p/~p]~n", [self(), Nodes]),
       timer:sleep(200),
       handling(Nodes)
   end.
 
 
-listener(Key) ->
+% listener(Key, Client)
+% Key     : Current key which we work on
+% Client  : Client who asks the information
+%
+% receiver to handle the response from the nodes/client
+% Quentin Nater - 12/21
+listener(Key, Client) ->
   receive
-    {_,ok} ->
-      listener(Key);
-    {Qref,keyok} ->
+    {l,_,ok} ->
+      listener(Key,Client);
+    {l,Qref,keyok} ->
       io:format("<< (~w) : The key ~w has been added~n", [Qref, Key]);
-    {Qref,R} ->
-      io:format("<< (~w) : The key ~w has the value ~w~n", [Qref, Key, R])
+    {l,Qref,R} ->
+      io:format("<< (~w) : The key ~w has the value ~w~n", [Qref, Key, R]),
+      {myring,Client} ! {lookkeys, Key, R}
+    after ?Timeout ->
+      io:format("Time out !~n")
   end.
 
-info() ->
-  self() ! info,
-  ok.
 
-lookFor(Key) ->
-  self() ! {h,lookfor,Key},
-  ok.
+% generateKey(Nodes)
+% Nodes : Array of all processes (nodes) inside the ring
+%
+% generate an unique and free key for the key and node IDs
+% Quentin Nater - 12/21
+generateKey(Nodes) ->
+  if length(Nodes) > ?Mbit -> % check if the ring has a place
+    io:format("<< (ERR) : The limit of nodes has been reached (~w)~n", [length(Nodes)]),
+    NewId = 0;
+  true ->
+    timer:sleep(100),
+    Id = key:generate(), % get a new key
+    Check = [Id || {Ids,_} <- Nodes, Ids == Id], % check if the key is already used
+    if length(Check) > 0 ->
+      NewId = (lists:last(Check) + length(Nodes)) rem ?Mbit;
+    true -> NewId = Id
+    end,
 
-addKey(Key, Value) ->
-  self() ! {h,addkey,Key,Value},
-  ok.
+    NewCheck = [NewId || {Ids,_} <- Nodes, Ids == NewId], % check if the new key is ok
 
-addNode(Key) ->
-  self() ! {h,addnode,Key},
-  ok.
+    if length(NewCheck) == 0 ->
+      ok;
+    true -> 
+      generateKey(Nodes) % retry
+    end
+  end,
+  io:format(">> (I) : choosen key (~w)~n", [NewId]),
+  NewId.
 
-
-lookFor(Key, Process) ->
-  node:getEntry(Key, Process, self()),
-  listener(Key),
-  m,n.
-
-addKey(Key, Value, Process) ->
-  node:addEntry(Key, Value, Process, self()),
-  listener(Key).
-
-addNode(Key, Process, Nodes) ->
-  Pid = node:deploy(Key, Process, []),
-  UpdatedNodes = [{Key,Pid}|Nodes],
-  UpdatedNodes.
-
-log(V, F, A) ->
-if 
-  ?VERBOSITY >= V -> io:format(F, A);
-  true -> true
-end.
+% generateKey()
+%
+% generate a key for the key and node IDs
+% Quentin Nater - 12/21
+generateKey() ->
+  Id = key:generate(),
+  Id.
 
 
+% stop(Nodes)
+% Nodes : Array of all processes (nodes) inside the ring
+%
+% Stop all processes
+% Quentin Nater - 12/21
 stop(Nodes) ->
     lists:foreach(fun({_,Node}) -> Node ! stop end, Nodes).
+
+
+% =========================================================== %
+% Interact with the system from outside                       %
+
+% CLIENT -> SERVER -> CLIENT %
+
+% receiver()
+%
+% Handle the message in the client side
+% Quentin Nater - 12/21
+receiver() ->
+  io:format(">> wait.....~n", []),
+  receive
+    {get,Nodes} ->
+      io:format(">> current nodes :~n"),
+      [io:format(">> ID : ~w / Process : ~w ~n", [X,Y]) || {X,Y} <- Nodes],
+      timer:sleep(200);
+    {lookkeys, Key, R} ->
+      io:format(">> The value of the key ~w is : ~w ~n", [Key, R]),
+      timer:sleep(200);
+    {get, keys, Key} ->
+      io:format(">> The key ~w has been added ~n", [Key]),
+      timer:sleep(200);
+    {getnodes, UpdatedNodes} ->
+      io:format(">> The new node has been added ~w~n", [UpdatedNodes]),
+      timer:sleep(200)
+    after ?Timeout ->
+      io:format("Time out !~n")
+  end.
+
+% init()
+%
+% Set the process name in the client side
+% Quentin Nater - 12/21 
+init() ->
+  register(myring, self()).
+
+
+% getInfo()
+% Host  :   Name of the server
+% Client:   Name of the client
+%
+% Ask the log of the ring
+% Quentin Nater - 12/21
+getInfo(Host, Client) ->
+  {myring,Host} ! {info, Client},
+  receiver(),
+  ok.
+
+
+% lookFor(Host, Key, Client)
+% Host  :   Name of the server
+% Key   :   Key to search
+% Client:   Name of the client
+%
+% Ask the value of a key and response to the client
+% Quentin Nater - 12/21
+lookFor(Host, Key, Client) ->
+  {myring,Host} ! {h, lookfor, Key, Client},
+  receiver(),
+  ok.
+
+
+% postKey(Host, Value, Client)
+% Host  :   Name of the server
+% Value :   Value of the new key
+% Client:   Name of the client
+%
+% Create a new key with automatic ID and response to the client
+% Quentin Nater - 12/21
+postKey(Host, Value, Client) ->
+  {myring,Host} ! {h, addkey, Value, Client},
+  receiver(),
+  ok.
+
+% postKey(Host, Key, Value, Client)
+% Host  :   Name of the server
+% Key   :   Id of the new key
+% Value :   Value of the new key
+% Client:   Name of the client
+%
+% Create a new key and response to the client
+% Quentin Nater - 12/21
+postKey(Host, Key, Value, Client) ->
+  {myring,Host} ! {h, addkey, Key, Value, Client},
+  receiver(),
+  ok.
+
+
+% login(Host, Client)
+% Host  :   Name of the server
+% Client:   Name of the client
+%
+% Create a new node/process with automatic Id and response to the client
+% Quentin Nater - 12/21
+login(Host, Client) ->
+  {myring,Host} ! {h, addnode, Client},
+  receiver(),
+  ok.
+
+% login(Host, Key, Client)
+% Host  :   Name of the server
+% Key   :   Id of the new node
+% Client:   Name of the client
+%
+% Create a new node/process and response to the client
+% Quentin Nater - 12/21
+login(Host, Key, Client) ->
+  {myring,Host} ! {h, addnode, Key, Client},
+  receiver(),
+  ok.
